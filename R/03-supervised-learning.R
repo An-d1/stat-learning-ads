1c369515e4b6dabfa352ad7a3dcde6077d187b40
Timur <arxteoki@gmail.com>
2026-03-10 17:26:22 +0100

    final final commit

--- R/03-supervised-learning.R
+++ R/03-supervised-learning.R
@@ -3,13 +3,17 @@ library(leaps)
 library(glmnet)
 library(pls)
 library(rpart)
+library(rpart.plot)
 library(randomForest)
 
-ads_data <- readRDS("../data/cleaned/global_ads_performance_dataset_cleaned.rds")
+ads_data <- readRDS("data/cleaned/global_ads_performance_dataset_cleaned.rds")
 ads_data$date <- NULL
 ads_data$ROAS <- NULL
 ads_data <- na.omit(ads_data)
 
+str(ads_data)
+
+# Train/test split
 set.seed(123)
 n <- nrow(ads_data)
 train_id <- sample(seq_len(n), size = floor(0.75 * n))
@@ -20,19 +24,17 @@ test_data  <- ads_data[-train_id, ]
 y_train <- train_data$revenue
 y_test  <- test_data$revenue
 
-
 #Linear model
 
 lm_full <- lm(revenue ~ ., data = train_data)
-print(summary(lm_full))
-print(vif(lm_full))
+summary(lm_full)
+vif(lm_full)
 
 lm_pred <- predict(lm_full, test_data)
 
 rmse_lm <- sqrt(mean((y_test - lm_pred)^2))
 r2_lm <- 1 - sum((y_test - lm_pred)^2) /
   sum((y_test - mean(y_test))^2)
-
 
 #Forward subset
 
@@ -51,8 +53,7 @@ plot(reg.summary$cp,
      ylab = "Cp")
 
 best_subset_size <- which.min(reg.summary$cp)
-cat("Best subset size:", best_subset_size, "\n")
-print(coef(regfit.fwd, best_subset_size))
+coef(regfit.fwd, best_subset_size)
 
 predict.regsubsets <- function(object, newdata, id){
   form  <- as.formula(object$call[[2]])
@@ -65,10 +66,10 @@ subset_pred <- predict.regsubsets(regfit.fwd,
                                   test_data,
                                   best_subset_size)
 
+
 rmse_subset <- sqrt(mean((y_test - subset_pred)^2))
 r2_subset <- 1 - sum((y_test - subset_pred)^2) /
   sum((y_test - mean(y_test))^2)
-
 
 #Ridge
 
@@ -76,12 +77,16 @@ x_train <- model.matrix(revenue ~ . -1, train_data)
 x_test  <- model.matrix(revenue ~ . -1, test_data)
 
 cv.ridge <- cv.glmnet(x_train, y_train, alpha = 0)
-
-plot(cv.ridge, main="Ridge - CV MSE")
+plot(cv.ridge)
+title("Ridge - CV MSE")
 
 ridge_pred <- predict(cv.ridge,
                       s = "lambda.min",
                       newx = x_test)
+
+rmse_ridge <- sqrt(mean((y_test - ridge_pred)^2))
+r2_ridge <- 1 - sum((y_test - ridge_pred)^2) /
+  sum((y_test - mean(y_test))^2)
 
 #Ridge coefficients
 
@@ -95,16 +100,19 @@ rmse_ridge <- sqrt(mean((y_test - ridge_pred)^2))
 r2_ridge <- 1 - sum((y_test - ridge_pred)^2) /
   sum((y_test - mean(y_test))^2)
 
-
 #Lasso
 
 cv.lasso <- cv.glmnet(x_train, y_train, alpha = 1)
-
-plot(cv.lasso, main="Lasso - CV MSE")
+plot(cv.lasso)
+title("Lasso - CV MSE")
 
 lasso_pred <- predict(cv.lasso,
                       s = "lambda.min",
                       newx = x_test)
+
+rmse_lasso <- sqrt(mean((y_test - lasso_pred)^2))
+r2_lasso <- 1 - sum((y_test - lasso_pred)^2) /
+  sum((y_test - mean(y_test))^2)
 
 #Lasso coefficients
 
@@ -118,7 +126,6 @@ rmse_lasso <- sqrt(mean((y_test - lasso_pred)^2))
 r2_lasso <- 1 - sum((y_test - lasso_pred)^2) /
   sum((y_test - mean(y_test))^2)
 
-
 #PCR
 
 pcr.fit <- pcr(revenue ~ .,
@@ -126,10 +133,20 @@ pcr.fit <- pcr(revenue ~ .,
                scale = TRUE,
                validation = "CV")
 
-validationplot(pcr.fit,
-               val.type = "MSEP",
-               main = "PCR - CV MSEP")
+msep_pcr <- RMSEP(pcr.fit)
+rmse_pcr_cv <- sqrt(msep_pcr$val[1,1,-1])
+best_pcr_comp <- which.min(rmse_pcr_cv)
 
+pcr_pred <- predict(pcr.fit,
+                    test_data,
+                    ncomp = best_pcr_comp)
+
+rmse_pcr <- sqrt(mean((y_test - pcr_pred)^2))
+r2_pcr <- 1 - sum((y_test - pcr_pred)^2) /
+  sum((y_test - mean(y_test))^2)
+
+validationplot(pcr.fit, val.type="MSEP", main = "PCR - CV MSEP")
+abline(v = best_pcr_comp, col="red", lwd=2)
 
 #PLS
 
@@ -138,10 +155,20 @@ pls.fit <- plsr(revenue ~ .,
                 scale = TRUE,
                 validation = "CV")
 
-validationplot(pls.fit,
-               val.type = "MSEP",
-               main = "PLS - CV MSEP")
+msep_pls <- RMSEP(pls.fit)
+rmse_pls_cv <- sqrt(msep_pls$val[1,1,-1])
+best_pls_comp <- which.min(rmse_pls_cv)
 
+pls_pred <- predict(pls.fit,
+                    test_data,
+                    ncomp = best_pls_comp)
+
+rmse_pls <- sqrt(mean((y_test - pls_pred)^2))
+r2_pls <- 1 - sum((y_test - pls_pred)^2) /
+  sum((y_test - mean(y_test))^2)
+
+validationplot(pls.fit, val.type="MSEP", main = "PLS - CV MSEP")
+abline(v = best_pls_comp, col="red", lwd=2)
 
 #Prunning
 
@@ -152,7 +179,8 @@ tree_model <- rpart(revenue ~ .,
                                             minsplit = 5,
                                             xval = 10))
 
-plotcp(tree_model, main="Tree - CV Error")
+plotcp(tree_model)
+title("Regression Tree - CV Error")
 
 best_cp <- tree_model$cptable[
   which.min(tree_model$cptable[,"xerror"]),
@@ -169,8 +197,10 @@ rmse_tree <- sqrt(mean((y_test - tree_pred)^2))
 r2_tree <- 1 - sum((y_test - tree_pred)^2) /
   sum((y_test - mean(y_test))^2)
 
+best_cp
+length(unique(pruned_tree$where))
 
-#Random forest
+#Random Forest
 
 rf_model <- randomForest(revenue ~ .,
                          data = train_data,
@@ -193,7 +223,6 @@ rmse_rf <- sqrt(mean((y_test - rf_pred)^2))
 r2_rf <- 1 - sum((y_test - rf_pred)^2) /
   sum((y_test - mean(y_test))^2)
 
-
 #Model comparison
 
 final_results <- data.frame(
@@ -209,16 +238,16 @@ final_results <- data.frame(
            rmse_subset,
            rmse_ridge,
            rmse_lasso,
-           rmse_tree,
-           rmse_rf,
+           rmse_pcr,
+           rmse_pls,
            rmse_tree,
            rmse_rf),
   R2 = c(r2_lm,
          r2_subset,
          r2_ridge,
          r2_lasso,
-         r2_tree,
-         r2_rf,
+         r2_pcr,
+         r2_pls,
          r2_tree,
          r2_rf)
 )
